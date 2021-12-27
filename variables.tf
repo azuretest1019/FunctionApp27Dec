a39d3fbca6aa125bf5d95ba5b82dffc8279d3237
azuretest1019 <94588868+azuretest1019@users.noreply.github.com>
2021-12-27 20:51:20 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable location {
+  default = "west europe"
+}
+
+variable resource_group {
+  default = "private-endpoint"
+}
+
+variable sa_name {
+  default = "privatecfm"
+}
+
+variable func_name {
+  default = "func-pe-test"
+}
+
+variable "sa_services" {
+  default = ["blob", "table", "queue", "file"]
+  type    = list
+}
+
+variable "sa_firewall_enabled" {
+  default = true
+  type    = bool
+}
